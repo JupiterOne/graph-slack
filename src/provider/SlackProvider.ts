86d8ae829258a57ebc494e5e3983161c08f76385
Nick Dowmon <nick.dowmon@jupiterone.com>
2020-11-23 17:08:06 -0500

    Add retries for platform errors

--- src/provider/SlackProvider.ts
+++ src/provider/SlackProvider.ts
@@ -1,3 +1,4 @@
+import { retry } from '@lifeomic/attempt';
 import { IntegrationLogger } from '@jupiterone/integration-sdk-core';
 import {
   WebClient,
@@ -5,6 +6,7 @@ import {
   WebAPICallResult,
   UsersListArguments,
   ConversationsListArguments,
+  ErrorCode,
 } from '@slack/web-api';
 import { SlackUser, SlackChannel } from './types';
 
@@ -24,16 +26,76 @@ export interface ListChannelsResult extends WebAPICallResult {
   channels: SlackChannel[];
 }
 
+const retryOptions = {
+  retries: 5,
+  factor: 1.75, // 1, 1.75, 3.06, 5.35, 9.38
+  minTimeout: 1000,
+  maxTimeout: 10000,
+  randomize: false,
+};
+
 export class SlackWebClient extends WebClient {
   private integrationLogger: IntegrationLogger;
 
-  constructor(
-    logger: IntegrationLogger,
-    token?: string,
-    options?: WebClientOptions,
-  ) {
-    super(token, options);
+  constructor(logger: IntegrationLogger, token: string) {
+    const webClientOptions: WebClientOptions = {
+      /**
+       * The slack client takes optional `logger` and `logLevel` arguments in order to
+       * print some of its own logs. We may want to enable this in the future.
+       */
+      // logger: {
+      //   ...logger,
+      //   getLevel: () => logLevel,
+      //   // eslint-disable-next-line @typescript-eslint/no-empty-function
+      //   setLevel: () => {},
+      //   // eslint-disable-next-line @typescript-eslint/no-empty-function
+      //   setName: () => {},
+      // },
+      // logLevel,
+      /**
+       * The Slack web client allows a set of retry options to retry errors thrown during
+       * axios.post requests.
+       *
+       * However, these retry configurations do not handle the following bad requests:
+       *   - 429: The Slack nodejs client parses the `retry-after` header and waits to retry
+       *
+       *   - !== 200: The Slack nodejs client states that "Slack's Web API doesn't use meaningful
+       *     status codes besides 429 and 200". When the API returns a non-200 status, the client
+       *     throws an error with the code "slack_webapi_platform_error". These errors are _not_
+       *     handled by the proceeding retry policy, and we explicitly handle these with a separate
+       *     slackWebApiPlatformErrorRetryPolicy.
+       *
+       * For more information, see the Slack nodejs client documentation:
+       * https://github.com/slackapi/node-slack-sdk/tree/main/packages/web-api
+       */
+      retryConfig: retryOptions,
+    };
+
+    super(token, webClientOptions);
     this.integrationLogger = logger;
+  }
+
+  async retryWebApiPlatformError<T>(callback: () => Promise<T>) {
+    return retry(callback, {
+      delay: retryOptions.minTimeout,
+      maxAttempts: retryOptions.retries,
+      jitter: retryOptions.randomize,
+      factor: retryOptions.factor,
+      minDelay: retryOptions.minTimeout,
+      maxDelay: retryOptions.maxTimeout,
+      handleError: (err, context) => {
+        if (err.code === ErrorCode.PlatformError) {
+          this.integrationLogger.warn(
+            {
+              err,
+            },
+            'API call returned retryable Slack Platform Error',
+          );
+        } else {
+          context.abort();
+        }
+      },
+    });
   }
 
   async iterateUsers(
@@ -44,16 +106,20 @@ export class SlackWebClient extends WebClient {
     let totalUsers = 0;
 
     do {
-      const listUsersResponse = await this.users.list({
-        cursor: nextCursor,
-        // Recommended limit max here: https://api.slack.com/methods/users.list
-        //
-        // If no limit is specified, then Slack will attempt to deliver the
-        // entire set. For large organizations, you may receive a "limit_required"
-        // error in the response or a 500 if no limit is specified.
-        limit: 200,
-        ...options,
-      });
+      const listUsersResponse = (await this.retryWebApiPlatformError(
+        async () => {
+          return this.users.list({
+            cursor: nextCursor,
+            // Recommended limit max here: https://api.slack.com/methods/users.list
+            //
+            // If no limit is specified, then Slack will attempt to deliver the
+            // entire set. For large organizations, you may receive a "limit_required"
+            // error in the response or a 500 if no limit is specified.
+            limit: 200,
+            ...options,
+          });
+        },
+      )) as WebAPICallResult & { members: any[] };
 
       const numUsersOnPage = listUsersResponse.members.length;
       this.integrationLogger.info(
@@ -67,7 +133,7 @@ export class SlackWebClient extends WebClient {
         await callback(user);
       }
 
-      nextCursor = listUsersResponse.response_metadata.next_cursor;
+      nextCursor = listUsersResponse.response_metadata!.next_cursor;
       totalUsers += listUsersResponse.members.length;
     } while (nextCursor);
 
@@ -87,14 +153,18 @@ export class SlackWebClient extends WebClient {
     let totalChannels = 0;
 
     do {
-      const listChannelsResponse = await this.conversations.list({
-        cursor: nextCursor,
-        // Recommended limit max here: https://api.slack.com/methods/conversations.list
-        //
-        // If no limit is specified, then Slack will default to 100.
-        limit: 200,
-        ...options,
-      });
+      const listChannelsResponse = (await this.retryWebApiPlatformError(
+        async () => {
+          return this.conversations.list({
+            cursor: nextCursor,
+            // Recommended limit max here: https://api.slack.com/methods/conversations.list
+            //
+            // If no limit is specified, then Slack will default to 100.
+            limit: 200,
+            ...options,
+          });
+        },
+      )) as WebAPICallResult & { channels: any[] };
 
       const numChannelsOnPage = listChannelsResponse.channels.length;
       this.integrationLogger.info(
@@ -108,7 +178,7 @@ export class SlackWebClient extends WebClient {
         await callback(channel);
       }
 
-      nextCursor = listChannelsResponse.response_metadata.next_cursor;
+      nextCursor = listChannelsResponse.response_metadata!.next_cursor;
       totalChannels += numChannelsOnPage;
     } while (nextCursor);
 
@@ -128,14 +198,18 @@ export class SlackWebClient extends WebClient {
     let totalChannelMembers = 0;
 
     do {
-      const listChannelMembersResponse = await this.conversations.members({
-        channel,
-        cursor: nextCursor,
-        // Recommended limit max here: https://api.slack.com/methods/conversations.members
-        //
-        // If no limit is specified, then Slack will default to 100.
-        limit: 200,
-      });
+      const listChannelMembersResponse = (await this.retryWebApiPlatformError(
+        async () => {
+          return this.conversations.members({
+            channel,
+            cursor: nextCursor,
+            // Recommended limit max here: https://api.slack.com/methods/conversations.members
+            //
+            // If no limit is specified, then Slack will default to 100.
+            limit: 200,
+          });
+        },
+      )) as WebAPICallResult & { members: any[] };
 
       const numChannelMembersOnPage = listChannelMembersResponse.members.length;
       this.integrationLogger.info(
@@ -149,7 +223,7 @@ export class SlackWebClient extends WebClient {
         await callback(member);
       }
 
-      nextCursor = listChannelMembersResponse.response_metadata.next_cursor;
+      nextCursor = listChannelMembersResponse.response_metadata!.next_cursor;
       totalChannelMembers += numChannelMembersOnPage;
     } while (nextCursor);
 
