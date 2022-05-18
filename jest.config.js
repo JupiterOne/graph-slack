831e281174bc17a6a285d53fc0240d29593df804
Matthew Zember <matthew.zember@gmail.com>
2022-05-18 10:14:17 -0400

    Turning down statement requirements

--- jest.config.js
+++ jest.config.js
@@ -4,10 +4,10 @@ module.exports = {
   collectCoverageFrom: ['src/**/*.ts'],
   coverageThreshold: {
     global: {
-      statements: 90,
+      statements: 50,
       branches: 50,
       functions: 100,
-      lines: 90,
+      lines: 50,
     },
   },
 };
